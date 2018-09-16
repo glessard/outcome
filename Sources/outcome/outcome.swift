4ede0e521bc51e4c6a1e16fe021a5948799c6f1f
Guillaume Lessard <dhtnstff@gmail.com>
2018-09-16 15:32:03 -0600

    copy Determined over from the Deferred package

--- Sources/outcome/outcome.swift
+++ Sources/outcome/outcome.swift
@@ -1,3 +1,102 @@
-struct outcome {
-    var text = "Hello, World!"
+//
+//  determined.swift
+//  deferred
+//
+//  Created by Guillaume Lessard on 9/26/17.
+//  Copyright © 2017 Guillaume Lessard. All rights reserved.
+//
+
+public struct Determined<Value>
+{
+  private let state: State<Value>
+
+  init(value: Value)
+  {
+    state = .value(value)
+  }
+
+  init(error: Error)
+  {
+    state = .error(error)
+  }
+
+  public func get() throws -> Value
+  {
+    switch state
+    {
+    case .value(let value): return value
+    case .error(let error): throw error
+    }
+  }
+
+  public var value: Value? {
+    if case .value(let value) = state { return value }
+    return nil
+  }
+
+  public var error: Error? {
+    if case .error(let error) = state { return error }
+    return nil
+  }
+
+  public var isValue: Bool {
+    if case .value = state { return true }
+    return false
+  }
+
+  public var isError: Bool {
+    if case .error = state { return true }
+    return false
+  }
 }
+
+#if swift (>=4.1)
+extension Determined: Equatable where Value: Equatable
+{
+  public static func ==(lhs: Determined, rhs: Determined) -> Bool
+  {
+    switch (lhs.state, rhs.state)
+    {
+    case (.value(let lhv), .value(let rhv)):
+      return lhv == rhv
+    case (.error(let lhe), .error(let rhe)):
+      return String(describing: lhe) == String(describing: rhe)
+    default:
+      return false
+    }
+  }
+}
+#endif
+
+#if swift (>=4.2)
+extension Determined: Hashable where Value: Hashable
+{
+  public func hash(into hasher: inout Hasher)
+  {
+    switch self.state
+    {
+    case .value(let value):
+      Int(0).hash(into: &hasher)
+      value.hash(into: &hasher)
+    case .error(let error):
+      Int(1).hash(into: &hasher)
+      String(describing: error).hash(into: &hasher)
+    }
+  }
+}
+#endif
+
+#if swift (>=4.2)
+@usableFromInline
+enum State<Value>
+{
+  case value(Value)
+  case error(Error)
+}
+#else
+enum State<Value>
+{
+case value(Value)
+case error(Error)
+}
+#endif
--- Tests/outcomeTests/outcomeTests.swift
+++ Tests/outcomeTests/outcomeTests.swift
@@ -1,15 +1,127 @@
-import XCTest
-@testable import outcome
+//
+//  DeterminedTests.swift
+//  deferred
+//
+//  Created by Guillaume Lessard on 6/5/18.
+//  Copyright © 2018 Guillaume Lessard. All rights reserved.
+//
 
-final class outcomeTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(outcome().text, "Hello, World!")
+import XCTest
+
+@testable import deferred
+
+class DeterminedTests: XCTestCase
+{
+  func testEquals1()
+  {
+#if swift (>=4.1)
+    let t1 = TBD<Int>()
+    let t2 = TBD<Int>()
+
+    var v1 = 0
+    var v2 = 0
+
+    t1.onValue { v1 = $0 }
+    t2.onValue { v2 = $0 }
+
+    let e = expectation(description: "equality test")
+
+    let t3 = t1.flatMap { i1 in t2.map { i2 in i1*i2 } }
+    t3.notify {
+      determined in
+      let ref = Determined(value: v1*v2)
+      XCTAssert(determined == ref)
+#if swift(>=4.2)
+      XCTAssert(determined.hashValue == ref.hashValue)
+#endif
+      e.fulfill()
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    t1.determine(nzRandom())
+    t2.determine(nzRandom())
+
+    waitForExpectations(timeout: 1.0)
+#endif
+  }
+
+  func testEquals2()
+  {
+#if swift (>=4.1)
+    let ev = nzRandom()
+
+    let t1 = TBD<Int>()
+    let t2 = t1.map { i -> Int in throw TestError(i) }
+
+    let e1 = expectation(description: "equality test a")
+
+    t1.notify {
+      determined in
+      let ref = Determined<Int>(error: TestError(ev))
+      XCTAssert(determined != ref)
+#if swift(>=4.2)
+      XCTAssert(determined.hashValue != ref.hashValue)
+#endif
+      e1.fulfill()
+    }
+
+    let e2 = expectation(description: "equality test b")
+
+    t2.notify {
+      determined in
+      let ref = Determined<Int>(error: TestError(ev))
+      XCTAssert(determined == ref)
+#if swift(>=4.2)
+      XCTAssert(determined.hashValue == ref.hashValue)
+#endif
+      e2.fulfill()
+    }
+
+    t1.determine(ev)
+
+    waitForExpectations(timeout: 1.0)
+#endif
+  }
+
+  func testHashable()
+  {
+#if swift(>=4.2)
+    let d1 = Determined<Int>(value: nzRandom())
+    let d2 = Determined<Int>(error: TestError(nzRandom()))
+
+    let set = Set([d1, d2])
+
+    XCTAssert(set.contains(d1))
+#endif
+  }
+
+  func testGetters() throws
+  {
+    let v1 = nzRandom()
+    let v2 = nzRandom()
+
+    let detValue = Determined<Int>(value: v1)
+    let detError = Determined<Int>(error: TestError(v2))
+
+    do {
+      let v = try detValue.get()
+      XCTAssert(v1 == v)
+
+      let _ = try detError.get()
+    }
+    catch let error as TestError {
+      XCTAssert(v2 == error.error)
+    }
+
+    XCTAssertNotNil(detValue.value)
+    XCTAssertNotNil(detError.error)
+
+    XCTAssertNil(detValue.error)
+    XCTAssertNil(detError.value)
+
+    XCTAssertTrue(detValue.isValue)
+    XCTAssertTrue(detError.isError)
+
+    XCTAssertFalse(detValue.isError)
+    XCTAssertFalse(detError.isValue)
+  }
 }
