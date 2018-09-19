a318c3f66662d00834e54fb01d423f4711441ab8
Guillaume Lessard <dhtnstff@gmail.com>
2018-09-19 09:11:36 -0600

    add a manually-built Xcode project

---
+++ Xcode/Outcome/Outcome.h
@@ -0,0 +1,19 @@
+//
+//  Outcome.h
+//  Outcome
+//
+//  Created by Guillaume Lessard on 9/19/18.
+//  Copyright © 2018 Guillaume Lessard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Outcome.
+FOUNDATION_EXPORT double OutcomeVersionNumber;
+
+//! Project version string for Outcome.
+FOUNDATION_EXPORT const unsigned char OutcomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Outcome/PublicHeader.h>
+
+
