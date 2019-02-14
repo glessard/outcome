55d7dfe9f183a67fb50393439f75277fc63b5fe2
Guillaume Lessard <dhtnstff@gmail.com>
2019-02-13 17:34:11 -0700

    Switch package manifests around

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,5 +12,5 @@ let package = Package(
     .target(name: "Outcome", dependencies: []),
     .testTarget(name: "OutcomeTests", dependencies: ["Outcome"]),
   ],
-  swiftLanguageVersions: [4]
+  swiftLanguageVersions: [.v4, .v4_2, .version("5")]
 )
--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,5 +12,5 @@ let package = Package(
     .target(name: "Outcome", dependencies: []),
     .testTarget(name: "OutcomeTests", dependencies: ["Outcome"]),
   ],
-  swiftLanguageVersions: [SwiftVersion.v4, .v4_2, .version("5")]
+  swiftLanguageVersions: [4]
 )
